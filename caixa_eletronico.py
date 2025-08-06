af0bc16d8f2d9f235ad977616774e017982d7d12
Michael <mackelldeveloper@gmail.com>
2025-08-05 23:36:56 -0300

    feature: corrigindo o código do desafio e implementando os conceitos da explicação

--- caixa_eletronico.py
+++ caixa_eletronico.py
@@ -1,9 +1,20 @@
 # Sistema Bancário - Caixa Eletrônico V2.0
-import datetime, time
+import datetime, time, textwrap
+from abc import ABC, abstractclassmethod, abstractproperty
 
 usuarios = []
 conta = []
 numero_conta = 1
+
+class ContaIterador:
+    def __init__(self, contas):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
 
 
 def limpar_tela():
@@ -33,6 +44,18 @@ def log_transacoes(func):
         print(f'\n[LOG] Transação: {func.__name__.upper()} | Data e Hora: {hora}')
         return resultado
     return wrapper
+
+
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self.contas = []
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
 
 
 @log_transacoes
@@ -177,3 +200,53 @@ while True:
 
     except ValueError:
         print('Entrada inválida. Por favor, digite um número.')
+
+
+class Historico:
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append({
+            "tipo": transacao.__class__.__name__,
+            "valor": transacao.valor,
+            "data": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+        })
+
+    def gerar_relatorio(self, tipo_transacao=None):
+        pass
+
+
+class Transacao(ABC):
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
+
+
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
